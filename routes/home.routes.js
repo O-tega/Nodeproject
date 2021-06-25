69fe74f1ec97c64fe28878cea87f294d61b969c1
Igho Oghenetega <tega.myview@gmail.com>
2021-06-25 09:56:43 +0100

    New practice for express

--- routes/home.routes.js
+++ routes/home.routes.js
@@ -1,59 +1,32 @@
-
+// import express and router
 const express = require('express');
 const router = express.Router();
-const multer = require('multer');
-const path = require('path')
 
-const storage = multer.diskStorage({
-    destination: (req, file, callback)=>{
-        callback(null, 'uploads/complain_img/')
-    },
+//import other files.
+const path = require('path');
 
-    filename: (req, file, callback)=>{
-        callback(null, `${Date.now()}-${file.originalname}`)
-    },
-})
-
-const upload = multer({storage, 
-    limits:{filesize:10000},
-    fileFilter:(req, file, callback)=>{
-        const extname = path.extname(file.originalname);
-        if(extname === '.jpg' || extname ==='.png' || extname ==='.txt'){
-            return callback(null, true);
-        }
-        return (callback(new Error('file type not supported'), false));
-    },
-}).single('file');
-
-router.get('/', function (req, res, next){
+//home routes
+router.get('/', (req, res, next) => {
     res.render('index.ejs', {
-        title: 'home',
-        server_url: req.server_url,
+        title: 'Home'
     });
 });
 
-router.get('/about', (req, res, next) =>{
+// about routes
+router.get('/about', (req, res, next) => {
     res.render('about.ejs', {
-        title:'about',
-        server_url: req.server_url,
+        title: 'About us'
     });
 });
 
+// contact routes
 router.get('/contact', (req, res, next) => {
     res.render('contact.ejs', {
-        title: 'contact',
-        server_url: req.server_url,
-    });
-});
-router.post('/complain', (req, res, next)=>{
-    upload(req, res, (err)=>{
-        if (err) {
-            return next(new Error(err));
-        }
-        res.send({body: req.body, file: req.file});
+        title: 'contact us'
     });
 });
 
 
 
-module.exports = router;
+
+module.exports = router
